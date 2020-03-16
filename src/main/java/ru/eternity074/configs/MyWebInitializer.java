c99ba6b776c796f26ee00eec344bffad9349422a
AK <AK@LAPTOP-R6BEK1CM>
2020-03-16 23:02:22 +0300

    Java config

--- src/main/java/ru/eternity074/configs/MyWebInitializer.java
+++ src/main/java/ru/eternity074/configs/MyWebInitializer.java
@@ -1,9 +1,19 @@
 package ru.eternity074.configs;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MyWebInitializer
 		extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		// TODO Auto-generated method stub
+		super.onStartup(servletContext);
+	}
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
--- src/main/java/ru/eternity074/configs/SpringWebMvcConfig.java
+++ src/main/java/ru/eternity074/configs/SpringWebMvcConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import ru.eternity074.dao.EmpDao;
+import ru.eternity074.dao.EmpDaoImpl;
 
 @EnableWebMvc
 @Configuration
@@ -25,7 +27,7 @@ public class SpringWebMvcConfig implements WebMvcConfigurer {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	
+
 	@Bean
 	public DriverManagerDataSource ds() {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
@@ -37,19 +39,25 @@ public class SpringWebMvcConfig implements WebMvcConfigurer {
 		ds.setPassword("1gjeOjuW");
 		return ds;
 	}
-	
+
 	@Bean
 	public JdbcTemplate jt() {
 		JdbcTemplate jt = new JdbcTemplate();
 		jt.setDataSource(ds());
 		return jt;
 	}
-	
+
 	@Bean
 	public EmpDao dao() {
-		EmpDao dao = new EmpDao();
+		EmpDaoImpl dao = new EmpDaoImpl();
 		dao.setTemplate(jt());
 		return dao;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/");
 	}
 
 }
